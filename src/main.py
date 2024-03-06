31bbcef94a8a3a6e551e6c556468420a20fb7a9a
JMR <joerg.mueller@ba-gmbh.com>
2024-03-06 08:05:13 +0100

    initial fast api template

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
